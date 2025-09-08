7fbbc0954a401f49552fc35e8588ad6890051d17
Luis Gomez <luisg31209@gmail.com>
2025-09-07 22:20:56 -0600

    push

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:8001';
+const API_BASE_URL = process.env.API_BASE_URL || 'http://192.168.1.7:8001';
 
 export default {
   API_BASE_URL,
